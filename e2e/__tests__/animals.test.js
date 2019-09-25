ef4bda88998588c405e079957fcd04709ac71649
olliecomet <leyechi@gmail.com>
2019-09-25 15:38:58 -0700

    initial setup

---
+++ e2e/__tests__/animals.test.js
@@ -0,0 +1,31 @@
+// const request = require('../request');
+const db = require('../db');
+
+describe('animals api', () => {
+  beforeEach(() => {
+    return db.dropCollection('animal');
+  });
+
+  // create an animal... copy from animal test...
+  // function postAnimal
+
+  it('posts an animal', () => {
+
+  });
+
+  it('gets an animal by ID', () => {
+
+  });
+
+  it('gets a list of animals', () => {
+
+  });
+
+  it('updates an animal', () => {
+
+  });
+
+  it('deletes an animal', () => {
+    
+  });
+});
--- server.js
+++ server.js
@@ -1,54 +1,11 @@
 require('dotenv').config();
-require('./lib/connect')();
+// require('./lib/connect')(process.env.MONGODB_URI);
 
-const express = require('express');
-const app = express();
-const Animal = require('./lib/lab-06/animal');
+const app = require('./lib/app');
+const { createServer } = require('http');
+const server = createServer(app);
 
-app.use(express.json());
-
-app.get('/api/animals', (req, res, next) => {
-  Animal.find()
-    .then(animals => {
-      res.json(animals);
-    })
-    .catch(next);
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
 });
-
-app.get('/api/animals/:id', (req, res, next) => {
-  Animal.findById(req.params.id)
-    .then(animal => {
-      res.json(animal);
-    })
-    .catch(next);
-});
-
-app.post('/api/animals', (req, res, next) => {
-  Animal.create(req.body)
-    .then(animal => {
-      res.json(animal);
-    })
-    .catch(next);
-});
-
-app.put('/api/animals/:id', (req, res, next) => {
-  Animal.findByIdAndUpdate(
-    req.params.id,
-    req.body,
-    { new: true }
-  )
-    .then(animal => {
-      res.json(animal);
-    })
-    .catch(next);
-});
-
-app.delete('/api/animals/:id', (req, res, next) => {
-  Animal.findByIdAndRemove(req.params.id)
-    .then(removed => {
-      res.json(removed);
-    })
-    .catch(next);
-});
-
-app.listen(3000, () => console.log('server running on 3000'));
